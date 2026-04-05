2d4ad408f2a26da058560d33310e2d36ed1cb925
saadimanar <manarsaadi187@gmail.com>
2026-04-05 16:25:42 +0300

    run build

--- app/api/chat/tools/getProjects.ts
+++ app/api/chat/tools/getProjects.ts
@@ -2,9 +2,9 @@ import { tool } from "ai";
 import { z } from "zod";
 
 export const getProjects = tool({
-  description: "This tool will show a list of all projects made by Raphael",
+  description: "This tool will show a list of all projects made by Manar",
   parameters: z.object({}),
   execute: async () => {
-    return "Here are all the projects made by Raphael (above)! Don't hesitate to ask me more about them!";
+    return "Here are all the projects made by Manar (above)! Don't hesitate to ask me more about them!";
   },
 });
